c9e6a2b3332a9007bd0c28db0be5f9f141f3cbb8
emcamguz2709 <emiliocamguz@gmail.com>
2026-03-08 20:49:31 -0600

    Update fraccion.h

--- fraccion/fraccion.h
+++ fraccion/fraccion.h
@@ -1,22 +1,15 @@
 #ifndef __FRACCION_H__
 #define __FRACCION_H__
 
-/* Tipo opaco */
-typedef struct Fraccion Fraccion;
+typedef struct{
+    int num;
+    int den;
+} Fraccion;
 
-/* Crear una fracción */
 Fraccion* crearFraccion(int num, int den);
-
-/* Simplificar la fracción */
-void simplificar(Fraccion* f);
-
-/* Sumar dos fracciones y regresar una nueva */
-Fraccion* sumar(Fraccion* a, Fraccion* b);
-
-/* Imprimir la fracción */
-void imprimir(Fraccion* f);
-
-/* Liberar memoria */
-void destruir(Fraccion* f);
+void simplificarFraccion(Fraccion* f);
+Fraccion* sumarFraccion(Fraccion *f1, Fraccion *f2);
+void imprimirFraccion(Fraccion *f);
+void destruirFraccion(Fraccion *f);
 
 #endif
